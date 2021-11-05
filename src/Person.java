527924c41e2b65ec24644c3cf03cb6ce7838844a
Luong Minh Tuan <tuan.luongminh@investidea.tech>
2021-11-05 11:13:56 +0700

    new issue 2

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,9 @@
 public class Person {
     public static void main(String[] args) {
         System.out.println("isue2");
+        System.out.println("fiasanonwifq");
+        System.out.println("fiasanonwifq");
+        System.out.println("fiasanonwifq");
+        System.out.println("fiasanonwifqqưeqweqeh");
     }
 }
